b0b6fa3457bd92ae50740597b2f6edd79279392a
cheny <cheyeon001214@naver.com>
2025-05-08 14:29:46 +0900

    feat: create Document in ReserveService

--- src/com/java/project01/service/impl/GHServiceImpl.java
+++ src/com/java/project01/service/impl/GHServiceImpl.java
@@ -1,5 +1,94 @@
 package com.java.project01.service.impl;
 
-public class GHServiceImpl {
+import java.util.List;
+
+import com.java.project01.service.GHService;
+import com.java.project01.util.MyDate;
+import com.java.project01.vo.Customer;
+import com.java.project01.vo.Reservation;
+import com.java.project01.vo.Room;
+import com.java.project01.vo.parent.Event;
+
+public class GHServiceImpl implements GHService{
+	
+	
+	
+	
+	//CRUD
+	@Override
+	public void Reservation(MyDate date, Customer customer, Room room, boolean isBreakfast, int people, Event event,
+			int reserveCode) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public List<Room> getAllRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public List<Event> getAllEvents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Reservation checkMyReserve(int code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void deleteReserve(int code) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void updateReserve(int code, Reservation reserve) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	//비지니스 로직
+
+	@Override
+	public Room mostPopularRoom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MyDate> soldOutDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Room> roomsByDate(MyDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Room> roomsByDate(MyDate date, int people) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Event> eventsByDate(MyDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBreakfastInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

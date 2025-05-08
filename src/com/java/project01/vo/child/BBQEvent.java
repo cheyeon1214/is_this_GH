d23c5d1c14438cb64fc33a05a8bbbc47df2f7f80
minho2618 <minho2618@naver.com>
2025-05-08 15:11:05 +0900

    Add: Event 자식 클래스 및 Room 클래스 JavaDoc 추가

--- src/com/java/project01/vo/child/BBQEvent.java
+++ src/com/java/project01/vo/child/BBQEvent.java
@@ -1,25 +1,55 @@
 package com.java.project01.vo.child;
 
 import com.java.project01.vo.parent.Event;
-
+/**
+ * BBQ 이벤트에 관한 클래스
+ */
 public class BBQEvent extends Event {
 
 	private String equipment;
 	private String food;
-
+	
+	public BBQEvent() {}
+	
+	/**
+	 * BBQEvent 생성자
+	 * @param equipment BBQ용 장비
+	 * @param food 음식
+	 */
+	public BBQEvent(String equipment, String food) {
+		this.equipment = equipment;
+		this.food = food;
+	}
+	
+	/**
+	 * BBQ용 장비 getter
+	 * @return BBQ용 장비 문자열
+	 */
 	public String getEquipment() {
 		return equipment;
 	}
 
-	public void setEquipment(String equipment) {
+	/**
+	 * BBQ용 장비 setter
+	 * @param equipment BBQ용 장비
+	 */
+	public void changeEquipment(String equipment) {
 		this.equipment = equipment;
 	}
 
+	/**
+	 * BBQ 음식 getter
+	 * @return 음식
+	 */
 	public String getFood() {
 		return food;
 	}
 
-	public void setFood(String food) {
+	/**
+	 * BBQ 음식 setter
+	 * @param food 음식
+	 */
+	public void changeFood(String food) {
 		this.food = food;
 	}
 
--- src/com/java/project01/vo/child/PartyEvent.java
+++ src/com/java/project01/vo/child/PartyEvent.java
@@ -1,24 +1,53 @@
 package com.java.project01.vo.child;
 
 import com.java.project01.vo.parent.Event;
-
+/**
+ * 파티와 관련된 클래스
+ */
 public class PartyEvent extends Event{
 
 	private String type;
 	private String food;
 	
+	public PartyEvent() {}
+	/**
+	 * PartyEvent 생성자
+	 * @param type 파티 유형
+	 * @param food 파티 음식
+	 */
+	public PartyEvent(String type, String food) {
+		this.type = type;
+		this.food = food;
+	}
+	
+	/**
+	 * 파타 유형을 반환하는 getter
+	 * @return 파티 유형 문자열
+	 */
 	public String getType() {
 		return type;
 	}
 	
-	public void setType(String type) {
+	/**
+	 * 파티 유형을 전환하는 setter
+	 * @param type 파티유형
+	 */
+	public void changeType(String type) {
 		this.type = type;
 	}
 	
+	/**
+	 * 파티 음식을 반환하는 getter
+	 * @return 파티 음식 문자열
+	 */
 	public String getFood() {
 		return food;
 	}
 	
+	/**
+	 * 파티 음식을 변경하는 setter
+	 * @param food 파티음식
+	 */
 	public void setFood(String food) {
 		this.food = food;
 	}

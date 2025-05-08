1479055d127c0c8f35a58ccc0a2b32091994095d
cheny <cheyeon001214@naver.com>
2025-05-08 14:58:46 +0900

    feat: Class Customer, Reservation, Room javaDoc,Constructor

--- src/com/java/project01/vo/Customer.java
+++ src/com/java/project01/vo/Customer.java
@@ -1,7 +1,7 @@
 package com.java.project01.vo;
 /**
  *  Customer Class
- *  - 고객의 정보 관련 클래스입니다.
+ *  - 고객 클래스입니다.
  *  
  */
 public class Customer {
@@ -10,25 +10,65 @@ public class Customer {
 	private char gender;
 	private String phoneNumber;
 	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
+	/**
+	 * 손님 생성자
+	 * @param name 손님의 이름
+	 * @param gender 손님의 성별
+	 * @param phoneNumber 손님의 전화번호
+	 */
+	public Customer(String name, char gender, String phoneNumber) {
+		super();
 		this.name = name;
-	}
-	public char getGender() {
-		return gender;
-	}
-	public void setGender(char gender) {
 		this.gender = gender;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 	
+	/**
+	 * 이름을 반환하는 메서드
+	 * @return 이름 문자열을 반환
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * 이름을 변경하는 메서드
+	 * @param name 변경할 이름
+	 */
+	public void changeName(String name) {
+		this.name = name;
+	}
+	/**
+	 * 성별을 반환하는 메서드 (f/m)
+	 * @return 여자는 f 남자는 m 반환
+	 */
+	public char getGender() {
+		return gender;
+	}
+	/**
+	 * 성별을 변경하는 메서드
+	 * @param gender 변경할 성별
+	 */
+	public void changeGender(char gender) {
+		this.gender = gender;
+	}
+	/**
+	 * 전화번호를 반환하는 메서드
+	 * @return 전화번호를 문자열로 반환
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * 전화번호를 변경하는 메서드
+	 * @param phoneNumber 변경할 전화번호 문자열
+	 */
+	public void changePhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	/**
+	 * 손님의 정보를 출력
+	 */
 	@Override
 	public String toString() {
 		return "Customer [name=" + name + ", gender=" + gender + ", phoneNumber=" + phoneNumber + "]";
--- src/com/java/project01/vo/Reservation.java
+++ src/com/java/project01/vo/Reservation.java
@@ -2,7 +2,11 @@ package com.java.project01.vo;
 
 import com.java.project01.util.MyDate;
 import com.java.project01.vo.parent.Event;
-
+/**
+ *  Reservation Class
+ *  - 예약 클래스입니다.
+ *  
+ */
 public class Reservation {
 	
 	private int reserveCode;
@@ -13,61 +17,128 @@ public class Reservation {
 	private int people;
 	private Event event;
 	
+	/**
+	 * 예약을 생성할 때 쓰이는 예약 생성자
+	 * @param reserveCode 예약 코드 
+	 * @param room 방 정보를 담은 방 객체 
+	 * @param date 예약 날짜 
+	 * @param customer 손님 정보를 담은 손님 객체 
+	 * @param isBreakfast 조식 유무 
+	 * @param people 인원수 
+	 */
+	public Reservation(int reserveCode, Room room, MyDate date, Customer customer, boolean isBreakfast, int people) {
+		super();
+		this.reserveCode = reserveCode;
+		this.room = room;
+		this.date = date;
+		this.customer = customer;
+		this.isBreakfast = isBreakfast;
+		this.people = people;
+	}
+
+	/**
+	 * 예약 코드를 반환함
+	 * @return 예약 코드
+	 */
 	public int getReserveCode() {
 		return reserveCode;
 	}
 	
-	public void setReserveCode(int reserveCode) {
-		this.reserveCode = reserveCode;
-	}
-	
+	/**
+	 * 방 객체를 반환함
+	 * @return 방 객체 
+	 */
 	public Room getRoom() {
 		return room;
 	}
 	
-	public void setRoom(Room room) {
+	/**
+	 * 예약한 방을 변경함 
+	 * @param room 변경할 방
+	 */
+	public void changeRoom(Room room) {
 		this.room = room;
 	}
 	
+	/**
+	 * 예약 날짜를 반환함 
+	 * @return 예약 날짜
+	 */
 	public MyDate getDate() {
 		return date;
 	}
 	
-	public void setDate(MyDate date) {
+	/**
+	 * 예약 날짜를 변경함 
+	 * @param date 변경할 날짜
+	 */
+	public void changeDate(MyDate date) {
 		this.date = date;
 	}
+	/**
+	 * 예약한 손님을 반환
+	 * @return 손님 반환
+	 */
 	public Customer getCustomer() {
 		return customer;
 	}
-	
-	public void setCustomer(Customer customer) {
+	/**
+	 * 손님 정보를 변경
+	 * @param customer 변경할 손님 정보를 담은 손님 객체 
+	 */
+	public void changeCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	
+	/**
+	 * 조식 유무를 반환함
+	 * @return true 이면 조식 YES, false 이면 조식 예약 NO
+	 */
 	public boolean isBreakfast() {
 		return isBreakfast;
 	}
-	
-	public void setBreakfast(boolean isBreakfast) {
+	/**
+	 * 조식 유무를 변경함 
+	 * @param isBreakfast 변경할 조식 유무 
+	 */
+	public void changeBreakfast(boolean isBreakfast) {
 		this.isBreakfast = isBreakfast;
 	}
 	
+	/**
+	 * 인원수를 반환
+	 * @return
+	 */
 	public int getPeople() {
 		return people;
 	}
 	
-	public void setPeople(int people) {
+	/**
+	 * 인원수 변경 
+	 * @param people 변경할 인원수 
+	 */
+	public void changePeople(int people) {
 		this.people = people;
 	}
 	
+	/**
+	 * 이벤트 반환 
+	 * @return 이벤트 
+	 */
 	public Event getEvent() {
 		return event;
 	}
 	
+	/**
+	 * 이벤트 추가
+	 * @param event 추가할 이벤트
+	 */
 	public void setEvent(Event event) {
 		this.event = event;
 	}
 
+	/**
+	 * 예약 정보를 출력
+	 */
 	@Override
 	public String toString() {
 		return "Reservation [reserveCode=" + reserveCode + ", room=" + room + ", date=" + date + ", customer="

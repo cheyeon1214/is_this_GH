8b029f0fe4a9988290e840ea1e1e422fd64d6bdf
minho2618 <minho2618@naver.com>
2025-05-12 09:24:48 +0900

    Feat: Cancel Reservation

--- src/com/java/project01/test/GHServiceTest.java
+++ src/com/java/project01/test/GHServiceTest.java
@@ -252,12 +252,19 @@ public class GHServiceTest {
 			}
 		}
 		
+		System.out.println("예약을 원하지 않으시면 -1을 입력하십시오.");
+		
 		//이 때 방 현황보고 뒤로가기 하는 것도 생각해야 함.
 		Room reserveRoom = null;
 		boolean validRoom = false;
 
 		while (!validRoom) {
 		    int roomChoice = sc.nextInt();
+		    
+		    if (roomChoice == -1) {
+		    	System.out.println("예약 선택을 취소합니다.");
+		    	return;
+		    }
 
 		    if (roomChoice < 1 || roomChoice > roomList.size()) {
 		        System.out.println("입력하신 번호의 방은 존재하지 않습니다. 다시 입력해주세요.");
